7ec88276a393b9c13540e75a0fa2d1acf7f25800
Gabriel Alfonso <gabrielalfonso99@gmail.com>
2020-05-06 15:08:30 -0400

    fixed the headers for Rating and Genre

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -20,119 +20,45 @@ class Pagination extends React.Component {
         this.state = { pager: {} };
     }
 
-    componentWillMount() {
-        // set page if items array isn't empty
-        if (this.props.pageNo && this.props.total) {
-            this.setPage(this.props.pageNo);
-        }
-    }
-
-    
-
-    setPage(page) {
-        var { total, pageSize } = this.props;
-        var pager = this.state.pager;
-
-        if (page < 1 || page > pager.totalPages) {
-            return;
-        }
-
-        // get new pager object for specified page
-        pager = this.getPager(total, page, pageSize);
-
-        // update state
-        this.setState({ pager: pager });
-
-        
-    }
-
-    getPager(totalItems, currentPage, pageSize) {
-        // default to first page
-        currentPage = currentPage || 1;
-
-        // default page size is 10
-        pageSize = pageSize || 10;
-
-        // calculate total pages
-        var totalPages = Math.ceil(totalItems / pageSize);
-
-        var startPage, endPage;
-        if (totalPages <= 10) {
-            // less than 10 total pages so show all
-            startPage = 1;
-            endPage = totalPages;
-        } else {
-            // more than 10 total pages so calculate start and end pages
-            if (currentPage <= 6) {
-                startPage = 1;
-                endPage = 10;
-            } else if (currentPage + 4 >= totalPages) {
-                startPage = totalPages - 9;
-                endPage = totalPages;
-            } else {
-                startPage = currentPage - 5;
-                endPage = currentPage + 4;
-            }
-        }
-
-        // calculate start and end item indexes
-        var startIndex = (currentPage - 1) * pageSize;
-        var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-        // create an array of pages to ng-repeat in the pager control
-        var pages = [...Array((endPage + 1) - startPage).keys()].map(i => startPage + i);
-
-        // return object with all pager properties required by the view
-        return {
-            totalItems: totalItems,
-            currentPage: currentPage,
-            pageSize: pageSize,
-            totalPages: totalPages,
-            startPage: startPage,
-            endPage: endPage,
-            startIndex: startIndex,
-            endIndex: endIndex,
-            pages: pages
-        };
-    }
-
     render() {
         //var pager = this.state.pager;
         var page = parseInt(this.props.pageNo);
 
 
         return (
-            <ul className="pagination">
-                <li className={page === 1 ? 'disabled' : ''}>
-                    {page === 1
-                    ? <a>First</a>
-                    : <a href={`${this.props.url}1`}>First</a>
-                    }   
-                </li>
-                <li className={page === 1 ? 'disabled' : ''}>
-                    {page === 1
-                    ? <a>Previous</a>
-                    : <a href={`${this.props.url}${page-1}`}>Previous</a>
-                    }
-                </li>
-                
-                <li className={page === page ? 'active' : ''}>
-                    <a> {this.props.pageNo}</a>
-                </li>
-                
-                <li className={page === this.props.total? 'disabled' : ''}>
-                    {page === this.props.total
-                    ? <a>Next</a>
-                    : <a href={`${this.props.url}${page+1}`}>Next</a>
-                    }
-                </li>
-                <li className={page === this.props.total ? 'disabled' : ''}>
-                    {page === this.props.total
-                    ? <a>Last</a>
-                    : <a href={`${this.props.url}${this.props.total}`}>Last</a>
-                    }
-                </li>
-            </ul>
+            <div className = "center2">
+                <ul className="pagination">
+                    <li className={page === 1 ? 'disabled' : ''}>
+                        {page === 1
+                        ? <a>First</a>
+                        : <a href={`${this.props.url}1`}>First</a>
+                        }   
+                    </li>
+                    <li className={page === 1 ? 'disabled' : ''}>
+                        {page === 1
+                        ? <a>Previous</a>
+                        : <a href={`${this.props.url}${page-1}`}>Previous</a>
+                        }
+                    </li>
+                    
+                    <li className={page === page ? 'active' : ''}>
+                        <a> {this.props.pageNo}</a>
+                    </li>
+                    
+                    <li className={page === this.props.total? 'disabled' : ''}>
+                        {page === this.props.total
+                        ? <a>Next</a>
+                        : <a href={`${this.props.url}${page+1}`}>Next</a>
+                        }
+                    </li>
+                    <li className={page === this.props.total ? 'disabled' : ''}>
+                        {page === this.props.total
+                        ? <a>Last</a>
+                        : <a href={`${this.props.url}${this.props.total}`}>Last</a>
+                        }
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
